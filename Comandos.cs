3bf3488f640f61bcdbadf460ad605f64feb14d3d
Caio Nolasco <kaiuviny@gmail.com>
2023-07-05 17:47:54 -0300

    Atualizacao do codigo com RPA Selenium

---
+++ Comandos.cs
@@ -0,0 +1,98 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Appium.Android;
+
+namespace InoveTeste
+{
+    class Comandos
+    {
+
+        #region Browser
+
+        public static IWebDriver GetBrowserLocal(IWebDriver driver, String browser)
+        {
+            switch (browser)
+            {
+                case "Internet Explorer":
+                    driver = new InternetExplorerDriver();
+                    driver.Manage().Window.Maximize();
+                    break;
+                case "Chrome":
+                    driver = new ChromeDriver();
+                    driver.Manage().Window.Maximize();
+                    break;
+                default:
+                    driver = new FirefoxDriver();
+                    driver.Manage().Window.Maximize();
+                    break;
+            }
+            return driver;
+        }
+
+        public static IWebDriver GetBrowserRemote(IWebDriver driver, String browser, String uri)
+        {
+            switch (browser)
+            {
+                case "Internet Explorer":
+                    InternetExplorerOptions cap_ie = new InternetExplorerOptions();
+                    driver = new RemoteWebDriver(new Uri(uri), cap_ie);
+                    driver.Manage().Window.Maximize();
+                    break;
+                case "Chrome":
+                    ChromeOptions cap_chrome = new ChromeOptions();
+                    driver = new RemoteWebDriver(new Uri(uri), cap_chrome);
+                    driver.Manage().Window.Maximize();
+                    break;
+                default:
+                    FirefoxOptions cap_firefox = new FirefoxOptions();
+                    driver = new RemoteWebDriver(new Uri(uri), cap_firefox);
+                    driver.Manage().Window.Maximize();
+                    break;
+            }
+            return driver;
+
+        }
+
+        public static IWebDriver GetBrowserMobile(IWebDriver driver, String platform, String deviceName, String browserName, String uri)
+        {
+            switch (platform)
+            {
+                case "Android":
+                    DesiredCapabilities cap_android = new DesiredCapabilities();
+                    cap_android.SetCapability("deviceName", deviceName);
+                    cap_android.SetCapability("browserName", browserName);
+                    driver = new AndroidDriver<IWebElement>(new Uri(uri), cap_android);
+                    break;
+                default:
+                    DesiredCapabilities cap_default = new DesiredCapabilities();
+                    cap_default.SetCapability("deviceName", deviceName);
+                    cap_default.SetCapability("browserName", browserName);
+                    driver = new AndroidDriver<IWebElement>(new Uri(uri), cap_default);
+                    break;
+            }
+            return driver;
+
+        }
+
+        #endregion
+
+
+        #region Javascript
+
+        public static void ExecuteJavascript(IWebDriver driver, String script)
+        {
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript(script);
+        }
+
+        #endregion
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("InoveTeste")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("InoveTeste")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("946e21d9-7a5d-4d5f-911b-60c6c6f4cacb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+//[assembly: Parallelizable(ParallelScope.Fixtures)]
+
